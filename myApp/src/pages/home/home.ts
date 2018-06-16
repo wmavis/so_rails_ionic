b8449f88d08bebd482d7fe375a71a945cb84ffde
Will Mavis <willard.mavis@gmail.com>
2018-06-15 21:36:22 -0500

    Create form for testing

--- myApp/src/pages/home/home.ts
+++ myApp/src/pages/home/home.ts
@@ -6,9 +6,22 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private obj;
 
   constructor(public navCtrl: NavController) {
+    var now = new Date();
+    console.log('toString', now.toString());
+    console.log('toISOString', now.toISOString());
+    console.log('toGMTString', now.toGMTString());
+    console.log('toLocaleString', now.toLocaleString());
+    this.obj = {
+      now: now.toISOString()
+    };
+    console.log(this.obj);
+  }
 
+  logForm() {
+    console.log(this.obj)
   }
 
 }
